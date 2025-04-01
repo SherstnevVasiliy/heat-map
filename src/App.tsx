e82bc9ed240b48012142b7e1dfcd0237aac949e4
vasiliy_sherstnev <vs@de.ru>
2025-04-02 02:06:19 +0300

    Изменение заголовка страницы на "Heat Map", добавление изображения "sample-image.webp" и обновление URL изображения в компоненте HeatMap.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="content">
         <h1>Тепловая карта кликов</h1>
         <HeatMap 
-          imageUrl="https://avatars.mds.yandex.net/i?id=656ac61f30ef121f5a0013cebfe7bf93_l-12569575-images-thumbs&n=13"
+          imageUrl="sample-image.webp"
           width={800}
           height={600}
         />
